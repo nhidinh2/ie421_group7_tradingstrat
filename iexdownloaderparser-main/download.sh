ed75b79304abfd21412fb2b302aa9e8c9bf27d66
Junsangparkk <143194675+Junsangparkk@users.noreply.github.com>
2024-12-16 09:53:58 +0900

    download.sh

--- iexdownloaderparser-main/download.sh
+++ iexdownloaderparser-main/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 src/download_iex_pcaps.py --start-date 2024-11-13 --end-date 2024-11-13 --download-dir data/iex_downloads
+python3 src/download_iex_pcaps.py --start-date 2024-11-14 --end-date 2024-11-14 --download-dir data/iex_downloads
